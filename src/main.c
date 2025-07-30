6a955e76f9ba71656a8eba13c3d7f1b4b07c3add
ysingh <ysingh0923@gmail.com>
2025-07-30 18:42:36 -0400

    Update

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #define YS_MATH_IMPLEMENTATION
-#include "ys_math.h"
+#include "../ys_base/src/ys_math.h"
+#include "../ys_base/src/ys_types.h"
+#include "../ys_base/src/ys_geom.h"
 
-int main(void) {
+i32 main(void) {
     return 0;
 }
